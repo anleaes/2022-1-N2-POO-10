c80ea214613381c8e02117f47fd3de095279b4e7
9rodrigosoares <9rodrigosoares@gmail.com>
2022-06-19 05:44:58 -0300

    classe usuario e classe cliente

---
+++ cliente.py
@@ -0,0 +1,17 @@
+class Cliente: 
+
+  def __init__(self, nome, cpf, endereco, telefone, usuario):
+  self.nome = nome
+  self._cpf = cpf
+  self.endereco = endereco
+  self.telefone = telefone
+  self.usuario = usuario
+
+  def personaliza_perfil(self):
+    return 'Alteração realizada!'
+
+  def altera_senha(self):
+    return 'Senha alterada!'
+
+  def novo_cliente(self):
+    return 'Registro concluído!'
